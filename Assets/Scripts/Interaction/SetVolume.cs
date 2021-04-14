17b3889a068f3a892ad8cfdd08218dcc3d7c4ed8
Reece <1022198@student.sae.edu.au>
2021-04-14 12:07:42 +1000

    Volume Fixes

--- Assets/Scripts/Interaction/SetVolume.cs
+++ Assets/Scripts/Interaction/SetVolume.cs
@@ -6,7 +6,7 @@ using UnityEngine.Audio;
 public class SetVolume : MonoBehaviour
 {
     public AudioMixer mixer;
-    
+
     public void SetLevel (float sliderValue)
     {
         mixer.SetFloat ("MusicVol", Mathf.Log10  (sliderValue) * 20);
