2510ae4e9352fbcb4e3fba616bf25594017dfcce
Hayden <whirefiregames1@hotmail.com>
2021-04-21 10:47:08 +1000

    speedup singleton

--- Assets/Scripts/Interaction/SpeedUp.cs
+++ Assets/Scripts/Interaction/SpeedUp.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class SpeedUp : MonoBehaviour
 {
+    #region Singleton
+    public static SpeedUp instance;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of speedup found");
+            return;
+        }
+        instance = this;
+    }
+#endregion
     // Update is called once per frame
     void Update()
     {
